71fd7d6e78953db6de14aa0878747410e3bce42d
1i6c04 <com45167@gmail.com>
2019-04-19 22:39:37 +0800

    檔案上傳

---
+++ src/main/java/jsfbeans/FileUploadController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsfbeans;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+import javax.faces.application.FacesMessage;
+import javax.inject.Named;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.event.FileUploadEvent;
+ 
+@Named("fileUploadController")
+public class FileUploadController {
+    private String destination = "C:\\Users\\erus\\Documents\\NetBeansProjects\\testProject\\src\\main\\webapp\\resources\\img";
+ 
+    public void upload(FileUploadEvent event) throws IOException {
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file
+        copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+ 
+    }
+ 
+    public void copyFile(String fileName, InputStream in) {
+        try {
+ 
+            // write the inputStream to a FileOutputStream
+            OutputStream out = new FileOutputStream(new File(destination + fileName));
+ 
+            int read = 0;
+            byte[] bytes = new byte[1024];
+ 
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+ 
+            in.close();
+            out.flush();
+            out.close();
+ 
+            System.out.println("New file created!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
